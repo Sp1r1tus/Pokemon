e9cc5db0147b0f4a3ded3bedd2d5693188e21f0e
Mathias Sperer <sp1r1tus@Mathiass-MacBook-Pro.local>
2020-02-29 22:24:40 +0100

    [MISC] CRA Boilerplate init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>a small Pokemon App</h1>
+      <p>here is my first paragraph</p>
+    </div>
+  );
+}
+
+export default App;
